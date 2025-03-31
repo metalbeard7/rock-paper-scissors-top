b30ca5ee3f411e83bfb203d4130584ca34e59722
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-03-30 21:59:04 -0400

    Remove console.log, add else if statements
    Removed test console.log message, and added else
    if statements to getComputerChoice function. else
    if statements are to tell function how to operate.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function getComputerChoice(){
    let a = Math.floor(Math.random() * 3) +1;
-    console.log(a);
+    
+   if (a <= 1) { 
+    console.log("Rock");
+   } else if (a <= 2) {
+    console.log("Scissors");
+   } else if (a <= 3) {
+    console.log("Paper");
+   }
+   return a;
+
 }
+ 
