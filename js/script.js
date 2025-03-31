fbca0f1e5e91df4b0378f0f405dc41702bd539e6
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-03-31 10:40:30 -0400

    Add func for round, and switch for each scenario
    Add function to play for a round, add switch
    statements for each match up in the game.

--- js/script.js
+++ js/script.js
@@ -22,5 +22,62 @@ function getHumanChoice() {
     
 }
 //compare answers and determine a winner or tie
+function playRound(humanChoice, computerChoice) {
+    let answer;
+    humanChoice = humanChoice.toLowerCase();
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("It is a tie, please try again.");
+                    answer = "tie";
+                    break;
+                case "paper":
+                    console.log("You lost, the computer has won.");
+                    answer = "lost";
+                    break;
+                case "scissors":
+                    console.log("YES! This is how we beat the machines!");
+                    answer = "win";
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("Analog for the win.");
+                    answer = "win";
+                    break;
+                case "paper":
+                    console.log("Nothing but knots, its a tie.");
+                    answer = "tie";
+                    break;
+                case "scissors":
+                    console.log("Oh no, you have been cut to shreds, you lost.");
+                    answer = "lost"
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("AI is smashing your dreams, you lost.");
+                    answer = "lost";
+                    break;
+                case "scissors":
+                    console.log("Oh baby, we could do this all day, we should tie the knot.");
+                    answer = "tie";
+                    break;
+                case "paper":
+                    console.log("Shred the competition! You won!");
+                    answer = "win";
+                    break;
+
+            }    
+            break;
+        }
+        return answer;
+    }
+    
 
 //create a round of play, increment score and continue to next round
