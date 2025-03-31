33f54a67152e0e9685e34f00c812a69e7146c7b6
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-03-30 22:14:28 -0400

    Add psuedocode comments and remove return a
    Added psuedocode for reference, removed return a.
    Return a was logging the generated number which
    was not necessary.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+//Create function to randomly generate a number between 1-3
+
 function getComputerChoice(){
    let a = Math.floor(Math.random() * 3) +1;
     
+
+   //tell function what to do with "number"
+        //if (a <= 1) log "Rock"
+        //if (a <= 2) log "Scissors"
+        //if (a <= 3) log "Paper"
+
    if (a <= 1) { 
     console.log("Rock");
    } else if (a <= 2) {
@@ -8,7 +16,6 @@ function getComputerChoice(){
    } else if (a <= 3) {
     console.log("Paper");
    }
-   return a;
 
 }
  
