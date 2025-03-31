f8181f9474b4c01926652b4e735577b8d14ca47f
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-03-31 00:13:21 -0400

    Add variables for score and initialize to 0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Create function to randomly generate a number between 1-3
+//create function to randomly generate a number between 1-3
 
 function getComputerChoice(){
    let a = Math.floor(Math.random() * 3) +1;
@@ -19,7 +19,7 @@ function getComputerChoice(){
 
 }
  
-//Create function to return a value based on user's input.
+//create function to return a value based on user's input.
     //Have user enter value in prompt.
         //if (choice = Rock) log "Rock"
         //if (choice <= Scissors) log "Scissors"
@@ -36,4 +36,9 @@ function getHumanChoice() {
     console.log("Paper");
    }
 }
+
+//declare starting score for human and computer.
+
+let humanScore = 0
+let computerScore = 0
 
