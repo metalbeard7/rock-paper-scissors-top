ea9ab5c93c15ed74813781848e308c08bd7c734c
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-03-31 20:52:01 -0400

    Add playGame function with game comp inside it
    Add the function that will encapsulate game
    mechanics.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-//declare a variable for playerScore and computerScore and initialize to 0
-let humanScore = 0
-let computerScore = 0
 
 //randomly generate the computers answer
 function getComputerChoice() {
@@ -37,94 +34,103 @@ for (let i=0; i<3; i++) {
     }
 }
 
-//create a round of play, increment score and continue to next round
 
-//compare answers and determine a winner or tie
-function playRound(humanChoice, computerChoice) {
-    let answer;
-    humanChoice = humanChoice.toLowerCase();
-      switch(humanChoice) {
-        case "rock":
-            switch(computerChoice) {
-                case "rock":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("It is a tie, please try again.");
-                    answer = "tie";
-                    break;
-                case "paper":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("You lost, the computer has won.");
-                    answer = "lost";
-                    break;
-                case "scissors":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("YES! This is how we beat the machines!");
-                    answer = "win";
-                    break;
-            }
-            break;
-        case "paper":
-            switch(computerChoice) {
-                case "rock":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("Analog for the win.");
-                    answer = "win";
-                    break;
-                case "paper":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("Nothing but knots, its a tie.");
-                    answer = "tie";
-                    break;
-                case "scissors":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("Oh no, you have been cut to shreds, you lost.");
-                    answer = "lost"
-                    break;
-            }
-            break;
-        case "scissors":
-            switch(computerChoice) {
-                case "rock":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("AI is smashing your dreams, you lost.");
-                    answer = "lost";
-                    break;
-                case "scissors":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("Oh baby, we could do this all day, we should tie the knot.");
-                    answer = "tie";
-                    break;
-                case "paper":
-                    console.log("human:", humanChoice);
-                    console.log("computer:", computerChoice);
-                    console.log("Shred the competition! You won!");
-                    answer = "win";
-                    break;
+//function for game that will house the rest of the game components.
+function playGame() {
 
-            }    
-            break;
+    //declare a variable for playerScore and computerScore and initialize to 0
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //create a round of play, increment score and continue to next round
+
+    //compare answers and determine a winner or tie
+    function playRound(humanChoice, computerChoice) {
+        let answer;
+        humanChoice = humanChoice.toLowerCase();
+        switch(humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("It is a tie, please try again.");
+                        answer = "tie";
+                        break;
+                    case "paper":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("You lost, the computer has won.");
+                        answer = "lost";
+                        break;
+                    case "scissors":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("YES! This is how we beat the machines!");
+                        answer = "win";
+                        break;
+                }
+                break;
+            case "paper":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("Analog for the win.");
+                        answer = "win";
+                        break;
+                    case "paper":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("Nothing but knots, its a tie.");
+                        answer = "tie";
+                        break;
+                    case "scissors":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("Oh no, you have been cut to shreds, you lost.");
+                        answer = "lost"
+                        break;
+                }
+                break;
+            case "scissors":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("AI is smashing your dreams, you lost.");
+                        answer = "lost";
+                        break;
+                    case "scissors":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("Oh baby, we could do this all day, we should tie the knot.");
+                        answer = "tie";
+                        break;
+                    case "paper":
+                        console.log("human:", humanChoice);
+                        console.log("computer:", computerChoice);
+                        console.log("Shred the competition! You won!");
+                        answer = "win";
+                        break;
+
+                }    
+                break;
+            }
+            return answer;
         }
-        return answer;
+
+    //const humanSelection = getHumanChoice();
+    //const computerSelection = getComputerChoice();
+
+
+
+    playRound(humanChoice, computerChoice);
+
+    if (humanScore > computerScore) {
+        console.log("You have WON the round!");
+    } else {
+        console.log("You have LOST the round.");
     }
-
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
-
-
-
-playRound(humanChoice, computerChoice);
-
-if (humanScore > computerScore) {
-    console.log("You have WON the round!");
-} else {
-    console.log("You have LOST the round.");
 }
-
+playGame();
